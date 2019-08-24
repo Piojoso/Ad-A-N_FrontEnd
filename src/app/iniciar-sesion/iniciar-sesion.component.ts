1516657c486e11635ebeb9e8ca03ffcf479f18f6
Leonel Bongiovanni (Piojoso) <labongiovanni@live.com.ar>
2019-08-24 20:32:02 -0300

    Iniciar-sesion.component utiliza el servicio
    Terminada los cambios al iniciar-sesion.component para que este
    trabaje con el nuevo servicio. Tambien se limpio del .html todo lo
    innecesario e igual del .ts.

--- src/app/iniciar-sesion/iniciar-sesion.component.ts
+++ src/app/iniciar-sesion/iniciar-sesion.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from '../usuario.service';
-import { Usuario } from '../usuario';
 import { Router } from '@angular/router';
+import { ShowErrorService } from '../show-error.service';
 
 @Component({
     selector: 'app-iniciar-sesion',
@@ -13,35 +13,22 @@ export class IniciarSesionComponent implements OnInit {
     email;
     pass;
 
-    err;
+    constructor(
+        private service:UsuarioService, 
+        private router:Router, 
+        private showError:ShowErrorService
+        ) { }
 
-    constructor(private service:UsuarioService, private router:Router) { 
-        /*
-        */
-    }
+    ngOnInit() { }
 
-    ngOnInit() {
-    }
-
-    showError(err){
-        this.err = null;
-        this.err = err;
-    }
-
-    close(){
-        this.err = null;
-    }
-
-    login(){
-        this.service.logIn({email: this.email, password: this.pass}).subscribe(data =>{
-            if(data) {
-                console.log(data);
+    login() {
+        this.service.logIn({email: this.email, password: this.pass}).subscribe(
+            data =>{
                 localStorage.setItem('token', data.token.toString());
                 this.router.navigateByUrl('/listar');
+            }, err =>{
+                this.showError.dispatchError(err);
             }
-        }, err =>{
-            this.showError(err);
-        })
-        
+        );
     }
 }
