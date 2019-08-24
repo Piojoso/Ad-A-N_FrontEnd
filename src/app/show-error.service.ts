d5e455b523b2e71b1235fa676fff281ac66283d1
Leonel Bongiovanni (Piojoso) <labongiovanni@live.com.ar>
2019-08-24 20:00:36 -0300

    Servicio showError finalizado

--- src/app/show-error.service.ts
+++ src/app/show-error.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShowErrorService {
 
+  private errors = [];
+  private errors$ = new BehaviorSubject<any[]>([]);
+
   constructor() { }
+
+  public select = () => [...this.errors];
+  public select$ = () => this.errors$.asObservable();
+
+  public dispatchError(error){
+    this.errors.push({...error});
+    this.errors$.next(this.select());
+  }
 }
