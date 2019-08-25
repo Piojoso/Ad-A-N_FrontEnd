cc849aea4a12e7685bec0ba7286c833e72cecce0
Leonel Bongiovanni (Piojoso) <labongiovanni@live.com.ar>
2019-08-25 02:27:56 -0300

    El componente subir usa el nuevo servicio
    Tambien agrege algunas notas para no olvidar revisar ciertas cosas
    que creo que son innecesarias.

--- src/app/subir/subir.component.ts
+++ src/app/subir/subir.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, NgZone } from '@angular/core';
 import { ArchivosService } from '../archivos.service';
 import { Router } from '@angular/router';
+import { ShowErrorService } from '../show-error.service';
 
 @Component({
   selector: 'app-subir',
@@ -14,12 +15,13 @@ export class SubirComponent implements OnInit {
 
   uploadedFiles: Array<File>;
 
-  constructor(private servicio:ArchivosService, private ngZone:NgZone, private router:Router) { }
+  // ngZone es realmente necesario?
+  constructor(private servicio:ArchivosService, private ngZone:NgZone, private router:Router, private showError: ShowErrorService) { }
   
   subir(){
     
     //Este codigo esta hecho para subir multiples archivos, pero la api no lo soporta, por lo que de momento, es solo de a un archivo a la vez.
-
+    // update: si hago qe se suban de a uno... quizas funcionaria, en vez de subir un array, voy subiendo de a un archivo por vez... tengo que verlo.
     let fromData = new FormData();
     for (var i = 0; i < this.uploadedFiles.length; i++){
       fromData.append("archivos", this.uploadedFiles[i], this.uploadedFiles[i].name);
@@ -27,16 +29,18 @@ export class SubirComponent implements OnInit {
     this.servicio.subirArchivo(fromData).subscribe(data =>{
       console.log(data);
       this.ngZone.run(()=>{this.router.navigateByUrl('/listar')})
-    })
-
+    }, err => {
+      this.showError.dispatchError(err);
+    });
   }
 
   fileChange(element){
     this.uploadedFiles = element.target.files;
+    // esto podria eliminarlo... Tengo que verlo.
   }
 
   cancel() {
-    this.uploadedFiles= new Array<File>();
+    this.uploadedFiles = new Array<File>();
   }
   
   ngOnInit() { }
