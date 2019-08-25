6644a040babd9388bdb5bf6948724f460af011b8
Leonel Bongiovanni (Piojoso) <labongiovanni@live.com.ar>
2019-08-25 02:18:11 -0300

    El componente registrarse usa el nuevo servicio

--- src/app/registrarse/registrarse.component.ts
+++ src/app/registrarse/registrarse.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UsuarioService } from '../usuario.service';
 import { Router } from '@angular/router';
+import { ShowErrorService } from '../show-error.service';
 
 @Component({
   selector: 'app-registrarse',
@@ -14,23 +15,31 @@ export class RegistrarseComponent implements OnInit {
   pass;
   confirm;
 
-  constructor(private service:UsuarioService, private router:Router) { }
+  constructor(
+    private service:UsuarioService, 
+    private router:Router, 
+    private showError: ShowErrorService) { }
 
   registrarse() {
     if(this.evaluar() == true){
-      this.service.logUp({email:this.email, userName: this.userName, password: this.pass}).subscribe(data => {
-        localStorage.setItem('token', data.token.toString());
-        this.router.navigateByUrl("/listar");
-      });
+      this.service.logUp({email:this.email, userName: this.userName, password: this.pass}).subscribe(
+        data => {
+          // Pronto el token dejara de guardarse en el local storage (Pienso ponerlo en un service y que de ahi lo obtengan todos los componentes.)
+          localStorage.setItem('token', data.token.toString());
+          this.router.navigateByUrl("/listar");
+        }, err => {
+          this.showError.dispatchError(err);
+        }
+      );
     }
   }
 
   evaluar(){
     if(this.pass = this.confirm) return true;
     else return false;
+    // DEBO ARREGLAR ESTO, hacerlo mejor, y mas interesante. este tipo de codigo ya no quiero hacerlo.
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
